70173a966706a8eb733c802fd7e116b0eb98f03b
Brandon R <brandonramosfreire@gmail.com>
2024-10-30 14:51:39 -0300

    atualização
    colocamos as imgs os sliders

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ var swiper = new Swiper('.heroSlider', {
     autoplay: true,
     speed: 2000,
 })
+
+// Product slide
+
+var swiper = new Swiper('.productSwiper', {
+    slidesPerView: 4, // Define o número de slides visíveis ao mesmo tempo
+    spaceBetween: 0, // Espaço entre os slides
+    loop: true,
+    autoplay: true,
+})
+
+// Brands slide
+
+var swiper = new Swiper('.brandSwiper', {
+    slidesPerView: 5, // Define o número de slides visíveis ao mesmo tempo
+    spaceBetween: 10, // Espaço entre os slides
+    loop: true,
+    autoplay: {
+        delay: 1500,
+    }
+})
