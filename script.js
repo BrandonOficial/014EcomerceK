a5966a9d661bdbd18b909b8ea62e630f74e31c51
Brandon R <brandonramosfreire@gmail.com>
2024-11-03 20:39:35 -0300

    concluido

--- script.js
+++ script.js
@@ -10,10 +10,24 @@ var swiper = new Swiper('.heroSlider', {
 // Product slide
 
 var swiper = new Swiper('.productSwiper', {
-    slidesPerView: 4, // Define o número de slides visíveis ao mesmo tempo
+    slidesPerView: 4, 
     spaceBetween: 0, // Espaço entre os slides
     loop: true,
     autoplay: true,
+    breakpoints: {
+        1200:{
+            slidesPerView: 4, 
+            spaceBetween: 10,
+        },
+        900:{
+            slidesPerView: 3, 
+            spaceBetween: 10,
+        },
+        500:{
+            slidesPerView: 2, 
+            spaceBetween: 20,
+        },
+    },
 })
 
 // Brands slide
@@ -24,5 +38,55 @@ var swiper = new Swiper('.brandSwiper', {
     loop: true,
     autoplay: {
         delay: 1500,
-    }
+    },
+    breakpoints: {
+        1200:{
+            slidesPerView: 4, 
+            spaceBetween: 10,
+        },
+        900:{
+            slidesPerView: 3, 
+            spaceBetween: 10,
+        },
+        500:{
+            slidesPerView: 2, 
+            spaceBetween: 20,
+        },
+    },
 })
+// Feature slide
+
+var swiper = new Swiper('.featureSwiper', {
+    slidesPerView: 3, // Define o número de slides visíveis ao mesmo tempo
+    spaceBetween: 10, // Espaço entre os slides
+    loop: true,
+    autoplay: true,
+    breakpoints: {
+        500:{
+            slidesPerView: 2, 
+            spaceBetween: 20,
+        },
+    },
+})
+
+
+// show mini products
+let miniProductsLink1 = document.querySelector('.mini_dot');
+let miniProductsLink2 = document.querySelector('.mini_dot2');
+let miniProduct = document.querySelector('.mini_product');
+let miniProduct2 = document.querySelector('.mini_product2');
+
+miniProductsLink1.addEventListener('click',() => {
+    miniProduct.classList.toggle('show_mini_product');
+});
+miniProductsLink2.addEventListener('click',() => {
+    miniProduct2.classList.toggle('show_mini_product');
+});
+
+// Show menu
+let bar = document.querySelector('.bars');
+let menu = document.querySelector('.menu');
+
+bar.addEventListener('click',() => {
+    menu.classList.toggle('show_menu');
+});
